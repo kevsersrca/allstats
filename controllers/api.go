49a82104b792c6c4cb2f73e80eaed5de06039562
Kevser <kevsrc@protonmail.com>
2019-05-16 02:36:50 +0300

    test

--- controllers/api.go
+++ controllers/api.go
@@ -4,33 +4,36 @@ import (
 	"allstats/lib"
 	"allstats/models"
 	"github.com/astaxie/beego/orm"
+	"encoding/json"
 )
 
 type ApiController struct {
 	BaseController
 }
 
+type test_struct struct {
+	Test string
+}
 
 func (c *ApiController) Agent() {
-	auth := c.GetString("post_data")
-	if auth == "" {
-		c.Data["json"] = lib.JsonData(false, "Auth required!", auth)
-		c.ServeJSON()
-		return
-	}
 
-	var task models.Task
-	task.Task = auth
-	task.Ongoing = true
-	_, err := orm.NewOrm().Insert(&task)
+	decoder := json.NewDecoder(c.Ctx.Input.Context.Request.Body)
+
+	var t test_struct
+	err := decoder.Decode(&t)
 	if err != nil {
-		c.Data["json"] = lib.JsonData(false, "Task not saved!", err)
+		c.Data["json"] = lib.JsonData(false, "Decode error", err.Error())
 		c.ServeJSON()
 		return
 	}
-	c.Data["json"] = lib.JsonData(true, "Success")
+	var task models.Task
+	task.Task = t.Test
+	task.Ongoing = true
+	orm.NewOrm().Insert(&task)
+	c.Data["json"] = lib.JsonData(true, "Success", t.Test)
 	c.ServeJSON()
 	return
+
 }
 func (c *ApiController) Create() {
 
